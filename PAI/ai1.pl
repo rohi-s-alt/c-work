b8aca9f40365b63c9decca2e2f1cb404c3a6490d
rohi-s-alt <120360975+rohi-s-alt@users.noreply.github.com>
2023-09-19 16:27:20 +0530

    Create ai1.pl

---
+++ PAI/ai1.pl
@@ -0,0 +1,16 @@
+% Mahabharata Family Tree
+wife(kauhshalya,dashrath).
+wife(kaikeyi, dashrath).
+wife(sumitra, dashrath).
+mother(kaushalya,ram).
+mother(kaikeyi,bharat).
+mother(sumitra,lakshman, shatrughan).
+wife(sita,ram).
+wife(mandvi,bharat).
+wife(urmila,lakshman).
+wife(shrutikeerti,shatrughan).
+mother(sita,lov,kush).
+mother(mandvi,taksha,pushkala).
+mother(urmila,angada,chandraketu).
+mother(shrutikeerti,subahu,shatrughati).
+mother(P,Q):-mother(P,Q,R);mother(P,R,Q).
