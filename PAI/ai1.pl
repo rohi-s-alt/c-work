453e7b2b476de1c262a80aa7e356727d8dabc6b3
rohi-s-alt <120360975+rohi-s-alt@users.noreply.github.com>
2023-09-19 16:57:24 +0530

    Update ai1.pl

--- PAI/ai1.pl
+++ PAI/ai1.pl
@@ -13,7 +13,13 @@ mother(sita,lov,kush).
 mother(mandvi,taksha,pushkala).
 mother(urmila,angada,chandraketu).
 mother(shrutikeerti,subahu,shatrughati).
-% ; ifelse
+% ; or/ifelse
 % , and
 mother(P,Q):-mother(P,Q,R);mother(P,R,Q).
 father(F,S):-wife(W,F),mother(W,S).
+husband(H,WW):-wife(WW,H).
+%son(SS,PP):-father(PP,SS);mother(PP,SS).
+son(SS,PP,MM):-father(PP,SS),mother(MM,SS).
+fatherinlaw(FIL,DD):-wife(DD,HH),father(FIL,HH).
+%motherinlaw(MIL,DD):-fatherinlaw(FF,DD),wife(MIL,FF).
+motherinlaw(MIL,DD):-wife(DD,HH),mother(MIL,HH).
