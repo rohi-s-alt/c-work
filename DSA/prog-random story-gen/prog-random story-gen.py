58e3cd9a6cab8cf3318c37b1ea3a7f238141eb74
rohi-s-alt <120360975+rohi-s-alt@users.noreply.github.com>
2023-10-17 11:48:38 +0530

    Create prog-random story-gen.py

---
+++ DSA/prog-random story-gen/prog-random story-gen.py
@@ -0,0 +1,6 @@
+# Importing random module 
+from random import choice as c
+from elements import *
+
+# Selecting an item from each list and concatenating them. 
+print(c(Sentence_starter) + c(character) + c(time) + c(story_plot) + c(place) + c(second_character) + c(age) + c(work))
