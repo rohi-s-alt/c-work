c04a97bd1587562523bbdc3088a41bb9beaafbb0
rohi-s-alt <120360975+rohi-s-alt@users.noreply.github.com>
2023-10-17 11:49:58 +0530

    Create elements.py

---
+++ DSA/prog-random story-gen/elements.py
@@ -0,0 +1,51 @@
+Sentence_starter = [
+    'About 100 years ago', 
+    'In the 20 BC', 
+    'Once upon a time', 
+    'In recent time'
+    ]
+ 
+character = [
+    ' there lived a king.', 
+    ' there was a man named Jack.', 
+    ' there lived a farmer.',
+    ' there was a student.'
+    ] 
+
+time = [
+    ' One day', 
+    ' One full-moon night',
+    ' Day before yesterday'
+    ]
+
+story_plot = [
+    ' he was passing by', 
+    ' he was going for a picnic to',
+    ' he was tired to go to'
+    ] 
+
+place = [
+    ' the mountains', 
+    ' the garden',
+    ' the school'
+    ] 
+
+second_character = [
+    ' he saw a man', 
+    ' he saw a young lady',
+    ' he saw his teacher'
+    ] 
+
+age = [
+    ' who seemed to be in late 20s', 
+    ' who seemed very old and feeble', 
+    ' who seemed very angry'
+    ]
+ 
+work = [
+    ' searching something.', 
+    ' digging a well on roadside.',
+    ' coming forward to scold him.'
+    ] 
+
+
