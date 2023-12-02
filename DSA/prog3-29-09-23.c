1355e80678f48c0c6da199256e2e0a89e0f4baaa
rohi-s-alt <120360975+rohi-s-alt@users.noreply.github.com>
2023-12-02 13:09:48 +0530

    Update prog3-29-09-23.c

--- DSA/prog3-29-09-23.c
+++ DSA/prog3-29-09-23.c
@@ -1,7 +1,7 @@
 // AIM - prog3 (8th of lab man) Create circular link list having information about a college and perform insertion at the front end and deletion at the end.
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct college {
 	int id;
@@ -15,12 +15,11 @@ S *ptr = NULL;
 S *fst = NULL;
 S *nw = NULL;
 
-
 void create() {
 	printf("Create And Insert Elements to the linked list\n");
-	
-	temp = (S*) malloc(sizeof(S*));
-	if(temp==NULL) {
+
+	temp = (S *)malloc(sizeof(S *));
+	if (temp == NULL) {
 		printf("No Space");
 		exit(0);
 	}
@@ -28,42 +27,41 @@ void create() {
 	scanf("%d", &temp->id);
 	printf("Enter College Name:");
 	scanf("%s", temp->name);
-	if (fst==NULL) {
+	if (fst == NULL) {
 		fst = temp;
 	} else {
-		ptr->nxt=temp;
+		ptr->nxt = temp;
 	}
 	temp->nxt = fst;
-	ptr=temp;
-	
+	ptr = temp;
 }
 
 void display() {
 	printf("\nDisplay the Linked List\n");
 	int count;
-	temp=fst;
-    if(temp==NULL) {
+	temp = fst;
+	if (temp == NULL) {
 		printf("\nList is Empty.\n");
 	} else {
-        do {
-            printf("\nRecord %d\n", count+1);
-            printf("College ID : %d\n", temp->id);
-            printf("College Name : %s\n", temp->name);
-            count++;
-            temp=temp->nxt;
-        } while(temp!=fst);
-    }	
+		do {
+			printf("\nRecord %d\n", count + 1);
+			printf("College ID : %d\n", temp->id);
+			printf("College Name : %s\n", temp->name);
+			count++;
+			temp = temp->nxt;
+		} while (temp != fst);
+	}
 }
 
 void insert() {
 	printf("insertion at the front\n");
-    pretemp=fst;
+	pretemp = fst;
 	do {
-		pretemp=pretemp->nxt;
-	} while(pretemp->nxt!=fst);
-	
-	nw = (S*) malloc(sizeof(S*));
-	if(nw==NULL) {
+		pretemp = pretemp->nxt;
+	} while (pretemp->nxt != fst);
+
+	nw = (S *)malloc(sizeof(S *));
+	if (nw == NULL) {
 		printf("No Space");
 		exit(0);
 	}
@@ -71,31 +69,30 @@ void insert() {
 	scanf("%d", &nw->id);
 	printf("Enter College Name:");
 	scanf("%s", nw->name);
-	
-    
-	nw->nxt=fst;
-    fst = nw;
-	pretemp->nxt=fst;
+
+	nw->nxt = fst;
+	fst = nw;
+	pretemp->nxt = fst;
 }
 
 void delete() {
-    printf("Deleting at the end\n");
-    pretemp=fst;
+	printf("Deleting at the end\n");
+	pretemp = fst;
 	do {
-		pretemp=pretemp->nxt;
-	} while((pretemp->nxt)->nxt!=fst);
-    printf("Element with college ID %d deleted successfully.\n", (pretemp->nxt)->id);
-    free(pretemp->nxt);
-    pretemp->nxt=fst;
+		pretemp = pretemp->nxt;
+	} while ((pretemp->nxt)->nxt != fst);
+	printf("Element with college ID %d deleted successfully.\n", (pretemp->nxt)->id);
+	free(pretemp->nxt);
+	pretemp->nxt = fst;
 }
 
-int main(){
+int main() {
 	int c;
 	do {
 		printf("Enter 1 to Create\nEnter 2 to display\nEnter 3 to insert at the front\nEnter 4 to delete at the end\nEnter 5 to Exit the Program\n");
 		printf("Input Choice (1,2,3,4,5) : ");
-		scanf("%d",&c);
-		switch(c) {
+		scanf("%d", &c);
+		switch (c) {
 			case 1:
 				create();
 				break;
@@ -105,12 +102,12 @@ int main(){
 			case 3:
 				insert();
 				break;
-            case 4:
-                delete();
-                break;
+			case 4:
+				delete ();
+				break;
 			default:
 				exit(0);
 		}
-	} while(1);
-    return 0;
+	} while (1);
+	return 0;
 }
