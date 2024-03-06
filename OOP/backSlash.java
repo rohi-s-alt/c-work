8488490e85300efe095ebaaa58610e450aedad1b
rohi-s-alt <120360975+rohi-s-alt@users.noreply.github.com>
2024-03-06 15:21:08 +0530

    Delete OOP/backSlash.java

--- OOP/backSlash.java
+++
@@ -1,9 +0,0 @@
-public class backSlash {
-    public static void main(String args[]){
-        System.out.println("Hello World");
-        System.out.println("\"AIDS in Java Class\"");
-        System.out.println("welcome \rAIDS");
-        System.out.println("\n Hello World");
-        System.out.println("\t Hello world");
-    }
-}
